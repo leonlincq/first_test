34f9f497ca8cfd19a3e84c6e6b733ffe62e52311
leonlincq <qilin-lingling@163.com>
2016-08-10 21:25:43 +0800

    Add files via upload

---
+++ main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  first_test
+//
+//  Created by leonlincq on 16/8/10.
+//  Copyright © 2016年 leonlincq. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
